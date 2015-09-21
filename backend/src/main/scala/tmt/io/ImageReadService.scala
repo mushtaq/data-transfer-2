4e648c8e1fb0e5a914c9f120ccd5e7060603c090
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-09-21 14:13:37 +0530

    set tick-duration in akka to 1 ms (default is 10ms)

--- backend/src/main/scala/tmt/io/ImageReadService.scala
+++ backend/src/main/scala/tmt/io/ImageReadService.scala
@@ -8,10 +8,11 @@ import akka.stream.io.SynchronousFileSource
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import tmt.app.{ActorConfigs, AppSettings}
-import tmt.common._
 import tmt.common.models.Image
+import tmt.library.SourceExtensions.RichSource
 
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationLong
 
 class ImageReadService(actorConfigs: ActorConfigs, settings: AppSettings, producer: Producer) {
   private val parallelism = 1
@@ -19,12 +20,11 @@ class ImageReadService(actorConfigs: ActorConfigs, settings: AppSettings, produc
   private def files = Source(() => producer.files(settings.framesInputDir))
 
   def sendBytes = files.mapAsync(parallelism)(readFile).map(ByteString.apply)
-  def sendImages = files.mapAsync(parallelism)(readImage)
+  def sendImages = files.mapAsync(parallelism)(readImage).throttle(settings.imageReadThrottle.millis)
   def sendMessages = files.map(SynchronousFileSource(_)).map(BinaryMessage.apply)
 
   private def readImage(file: File) = readFile(file).map(data => Image(file.getName, data))(actorConfigs.ec)
   private def readFile(file: File) = Future {
-    Thread.sleep(settings.imageReadThrottle)
     Files.readAllBytes(file.toPath)
   }(settings.fileIoDispatcher)
 }
